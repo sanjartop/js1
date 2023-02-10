6fc7843d85567cba2f6c7518a90352ccba2a709b
smailkusnanjar97 <smailkulovsanjar97@gmail.com>
2023-02-10 14:17:01 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+
+// let name = prompt('Введите свое имя')
+// let lastname = prompt('введите вашу фамилию')
+
+// alert('ПРивет'+ ' ' +  name  + ' ' + lastname)
+
+// 2
+// let num1 = prompt ('Введитее первое число')
+// let num2 = prompt ('Введитее второе число')
+
+// if (num1 > num2){
+//     alert(num1 + ' ' + 'Больше' + ' ' + num2)
+// }else if(num1 < num2){
+//     alert(num1 + ' ' + 'Меньше' + ' ' + num2)
+// }else{
+//     alert('Они равны')
+// }
+
+//3)
+// let color = prompt('Введите цвет...')
+
+// switch (color) {
+//     case ('red'):
+//         alert('Стоп')
+//         break;
+//     case('yellow'):
+//     alert('Приготовиться')
+//     break;
+//     case('green'):
+//     alert('Езжайте')
+//     break;
+//     default:
+//         alert('Такого цвета нету')
+//         break;
+// }
